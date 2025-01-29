269993d2e5168932c3d07f1af3ab8bf87d422ad9
shaustruk_k <es@banksiaglobal.com>
2025-01-29 15:58:00 +0100

    init: init app

---
+++ pg/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PG',
+  webDir: 'www'
+};
+
+export default config;
