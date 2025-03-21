90addf36ff23711a878c38de4d1fece1d5c463c5
playger-main <playgroundmanage@gmail.com>
2025-03-22 01:10:39 +0200

    reinstall ionic

--- pg/src/app/tabs/tabs.page.ts
+++ pg/src/app/tabs/tabs.page.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { triangle, ellipse, square } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  standalone: false,
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
+  public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {}
-
+  constructor() {
+    addIcons({ triangle, ellipse, square });
+  }
 }
