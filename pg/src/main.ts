744c22dc71e1d869bb354122bc10c3f4ff9b0136
playger-main <playgroundmanage@gmail.com>
2025-03-22 01:45:50 +0200

    remove files

--- pg/src/main.ts
+++ pg/src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { routes } from './components/app/app.routes';
+import { AppComponent } from './components/app/app.component';
 
+defineCustomElements(window);
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
