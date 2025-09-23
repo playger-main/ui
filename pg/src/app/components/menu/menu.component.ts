25bcfaa460e74decf12cf4e21200f5e82ef28fff
shaustruk_k <es@banksiaglobal.com>
2025-09-23 22:43:27 +0200

    fix: add items for menu

--- pg/src/app/components/menu/menu.component.ts
+++ pg/src/app/components/menu/menu.component.ts
@@ -7,15 +7,27 @@ import {
   IonTitle,
   IonToolbar,
   MenuController,
+  IonIcon,
 } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import { addIcons } from 'ionicons';
+import {
+  homeOutline,
+  football,
+  settingsOutline,
+  cashOutline,
+  chatbubbleOutline,
+  notificationsOutline,
+  gridOutline,
+  balloonOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonItem, IonList, RouterModule],
+  imports: [IonItem, IonList, RouterModule, IonIcon],
 })
 export class MenuComponent {
   constructor(
@@ -23,6 +35,16 @@ export class MenuComponent {
     private appComponent: AppComponent,
   ) {
     console.log('menu component');
+    addIcons({
+      homeOutline,
+      chatbubbleOutline,
+      cashOutline,
+      football,
+      settingsOutline,
+      notificationsOutline,
+      gridOutline,
+      balloonOutline,
+    });
   }
   handlerMenu(title: string) {
     this.menu.close();
